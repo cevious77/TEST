39ec8c21ad707537c01001a3853cdf0abf1b6eca
Marot <48023487+cevious77@users.noreply.github.com>
2019-03-29 17:05:35 +0100

    yallah
    oui monsieur

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -3,28 +3,52 @@ import React from "react"
 import "./Travel.css"
 
 
-const Travel = props => {
-   return <div>
-       
-       <figure>
-           <p> {props.destination} </p>
-    <img
-      src= {props.photo}
-      alt={props.distance}
-    />
-    <figcaption>
-      <blockquote>
-        {props.country}
-      </blockquote>
-      <cite>{props.distance}</cite>
-    </figcaption>
-  </figure>
+    
+    const Travel = ({ destination, country, photo, distance }) => { 
+        return <div>
+           <div> {destination} </div>
+           <div>{country}  </div> 
+            <div> <img src={photo}/> </div> 
+          <div> {distance} </div>  
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+             </div>
+    }
 
-   </div>
+
+
+
+
+
+
+
+
+// const Travel = props => {
+//    return <div>
+       
+//        <figure>
+//            <p> {props.destination} </p>
+//     <img
+//       src= {props.photo}
+//       alt={props.distance}
+//     />
+//     <figcaption>
+//       <blockquote>
+//         {props.country}
+//       </blockquote>
+//       <cite>{props.distance}</cite>
+//     </figcaption>
+//   </figure>
+
+//    </div>
 
    
 
-}
+//}
 
 export default Travel
 
